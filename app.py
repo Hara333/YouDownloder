f3e006f7f61c73612c5512985f4094e280ce0d53
Hara333 <sokeangmeng@gmail.com>
2024-05-05 16:46:15 +0700

    a

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from pytube import YouTube
 from pytube.cli import on_progress
 
 link = 'https://www.youtube.com/watch?v=_4_Iwg17ehs'
-yt = YouTube(link, on_progress_callback=on_progress())
-print('\n', yt.streams.filter(res='720p'))
-steam = yt.streams.get_by_itag(22)
-steam.download(output_path="/Users/hara/Desktop/DL")
+yt = YouTube(link)
+
+print('\n', yt.streams.order_by('resolution').desc().first())
+#steam.download(output_path='/Users/hara/Desktop/DL')
--- playground.py
+++ playground.py
@@ -1,11 +1,14 @@
+import os.path
+import os
 import customtkinter as ctk
 
 root = ctk.CTk()
+root.geometry('480x480')
 
 colors = ["Red", "Green", "Blue"]
 option1 = ctk.CTkOptionMenu(root, values=colors)
 
+print(os.getenv('HOME'))
 
-#run
 option1.pack(pady=10)
 root.mainloop()
