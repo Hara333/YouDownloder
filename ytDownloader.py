15e96a7041c96149ae49404e7396b58822874b1b
Hara333 <sokeangmeng@gmail.com>
2024-05-07 16:42:22 +0700

    v1.10 removed tab view

--- ytDownloader.py
+++ ytDownloader.py
@@ -5,7 +5,7 @@ import requests
 import datetime
 
 window = ctk.CTk()
-window.geometry('720x480')
+window.geometry('720x720')
 window.title('YouTube Downloader')
 
 width = 360
@@ -23,14 +23,29 @@ def on_progress(steam, chunk, byte_remaining):
 def clicker():
     link = url_entry.get()
     yt = YouTube(link, on_progress_callback=on_progress)
+
+    global steam_mp3
+    global steam_mp4
     info_label.configure(text=yt.title)
     duration_label.configure(text="Duration: " + str(datetime.timedelta(seconds=yt.length)))
     label_image.configure(image=image_thumbnail, width=width, height=height)
-    image_thumbnail.configure(light_image=Image.open(requests.get(yt.thumbnail_url, stream=True).raw),size=(width,height))
-    #steam = yt.streams.filter(res=reso_box, progressive=True).first()
-    #steam = yt.streams.order_by().desc().first()
-    #steam = yt.streams.get_by_resolution(reso_box.get())
-    #steam.download(output_path='/Users/hara/Desktop/DL')
+    image_thumbnail.configure(light_image=Image.open(requests.get(yt.thumbnail_url,stream=True).raw),size=(width,height))
+
+    steam_mp3 = yt.streams.filter(only_audio=True).desc().first()
+    btn_mp3.configure(text=str(steam_mp3)[47:55].replace('"','') + " | " + str(steam_mp3.filesize_mb) +" MB")
+    label_mp3.pack()
+    btn_mp3.pack()
+
+    steam_mp4 = yt.streams.filter(res='720p').desc().first()
+    btn_mp4.configure(text=str(steam_mp4)[47:53].replace('"','') + " | " + str(steam_mp4.filesize_mb) +" MB")
+    label_mp4.pack()
+    btn_mp4.pack()
+
+
+
+def mp3_dl():
+
+
 
 #thumbnail_image
 image_thumbnail = ctk.CTkImage(light_image=Image.open("assets/img/youtube_logo.jpg"), size=(width,height))
@@ -40,30 +55,10 @@ url_label = ctk.CTkLabel(window, text='Enter URL')
 url_entry = ctk.CTkEntry(window, width=width)
 link = url_entry.get()
 
-#select_resolution
-#option = ['360p','480p','720p']
-tab_select = ctk.CTkTabview(window, width=200, height=250)
-tab_mp3 = tab_select.add('Mp3')
-
-btn_320 = ctk.CTkButton(tab_mp3,text="(.mp3) | 320kbps")
-btn_256 = ctk.CTkButton(tab_mp3,text="(.mp3) | 256kbps")
-btn_192 = ctk.CTkButton(tab_mp3,text="(.mp3) | 192kbps")
-btn_128 = ctk.CTkButton(tab_mp3,text="(.mp3) | 128kbps")
-btn_64 = ctk.CTkButton(tab_mp3,text="(.mp3) | 64kbps")
-btn_320.pack()
-btn_256.pack(pady=5)
-btn_192.pack()
-btn_128.pack(pady=5)
-btn_64.pack()
-
-tab_mp4 = tab_select.add('Mp4')
-
-btn_360 = ctk.CTkButton(tab_mp4,text="(.mp4) | 1080p")
-btn_720 = ctk.CTkButton(tab_mp4,text="(.mp4) | 720p")
-btn_1080 = ctk.CTkButton(tab_mp4,text="(.mp4) | 360p")
-btn_360.pack()
-btn_720.pack(pady=5)
-btn_1080.pack()
+label_mp3 = ctk.CTkLabel(window, text='mp3')
+label_mp4 = ctk.CTkLabel(window, text='mp4')
+btn_mp3 = ctk.CTkButton(window, text='', command=mp3_dl)
+btn_mp4 = ctk.CTkButton(window, text='')
 
 #reso_box = ctk.CTkOptionMenu(window, values=option)
 
@@ -72,22 +67,20 @@ dl_progress = ctk.CTkProgressBar(window, width=width)
 dl_progress.set(0)
 
 #Button Download
-dl_btn = ctk.CTkButton(window, text='DOWNLOAD', command=clicker)
+dl_btn = ctk.CTkButton(window, text='Submit', command=clicker)
 
 percen_label = ctk.CTkLabel(window, text=str(dl_progress.get()) + '%')
-info_label = ctk.CTkLabel(window, text='Title')
-duration_label = ctk.CTkLabel(window, text='Duration')
+info_label = ctk.CTkLabel(window, text='')
+duration_label = ctk.CTkLabel(window, text='')
 
 #run
 url_label.pack(pady=10)
 url_entry.pack(pady=10)
+dl_btn.pack(pady=10)
 label_image.pack()
-tab_select.pack()
 info_label.pack(pady=5)
 duration_label.pack(pady=5)
-#reso_box.pack(pady=10)
-dl_btn.pack(pady=10)
-dl_progress.pack(pady=10)
-percen_label.pack()
+# dl_progress.pack(pady=10)
+# percen_label.pack()
 
 window.mainloop()
